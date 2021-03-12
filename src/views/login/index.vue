a0b1b1ef2221c4ddd8a167d3a294c81303bd3663
lizhiquan <zhiquan.li@gl-inet.com>
2021-03-12 15:58:15 +0800

    登录界面背景色动态切换

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -100,7 +100,6 @@ export default {
 <style lang="less" scoped>
 .login-container {
   height: 100%;
-  background-color: rgb(75, 124, 189);
   position: relative;
   .loginFrom-container {
     width: 500px;
